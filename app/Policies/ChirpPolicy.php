5e237ba757a87ae05f7929f15816d6a0fc812e2a
Yanes Abdelkader <74564722+YanesAbdelkader@users.noreply.github.com>
2025-10-07 20:34:02 +0100

    Update ChirpPolicy.php

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -35,17 +35,15 @@ class ChirpPolicy
     /**
      * Determine whether the user can update the model.
      */
+    
     public function update(User $user, Chirp $chirp): bool
     {
-        return false;
+        return $chirp->user()->is($user);
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, Chirp $chirp): bool
     {
-        return false;
+        return $chirp->user()->is($user);
     }
 
     /**
